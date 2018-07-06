826045fe645c37c2fa9597e966bfcaf94407d9aa
Francesco Romani <fromani@redhat.com>
2018-07-06 12:53:09 +0200

    tests: bootstrap tests
    Signed-off-by: Francesco Romani <fromani@redhat.com>

---
+++ pkg/vmmi/messages/messages_test.go
@@ -0,0 +1,103 @@
+package messages
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"testing"
+)
+
+func TestReportSuccess(t *testing.T) {
+	var W bytes.Buffer
+	var L bytes.Buffer
+	sink := Sink{
+		W: &W,
+		L: log.New(&L, "test: ", log.LstdFlags),
+	}
+	sink.ReportSuccess()
+
+	var msg CompletionSuccess
+	dec := json.NewDecoder(&W)
+	dec.Decode(&msg)
+
+	if msg.Version != Version {
+		t.Errorf("wrong version: %v vs %v", msg.Version, Version)
+	}
+	if msg.ContentType != ContentTypeCompletion {
+		t.Errorf("wrong contentType: %v vs %v", msg.ContentType, ContentTypeCompletion)
+	}
+	if msg.Timestamp <= 0 {
+		t.Errorf("wrong timestamp: %v", msg.Timestamp)
+	}
+
+	if msg.Completion.Result != CompletionResultSuccess {
+		t.Errorf("wrong completion result: %v vs %v", msg.Completion.Result, CompletionResultSuccess)
+	}
+}
+
+func TestReportError(t *testing.T) {
+	var W bytes.Buffer
+	var L bytes.Buffer
+	sink := Sink{
+		W: &W,
+		L: log.New(&L, "test: ", log.LstdFlags),
+	}
+	errData := ErrorData{
+		Code:    4242,
+		Message: "testing error message",
+		Details: "testing error message details, slightly more verbose",
+	}
+	sink.ReportError(errData.Code, errData.Message, errData.Details)
+
+	var msg CompletionError
+	dec := json.NewDecoder(&W)
+	dec.Decode(&msg)
+
+	if msg.Version != Version {
+		t.Errorf("wrong version: %v vs %v", msg.Version, Version)
+	}
+	if msg.ContentType != ContentTypeCompletion {
+		t.Errorf("wrong contentType: %v vs %v", msg.ContentType, ContentTypeCompletion)
+	}
+	if msg.Timestamp <= 0 {
+		t.Errorf("wrong timestamp: %v", msg.Timestamp)
+	}
+
+	if msg.Completion.Result != CompletionResultError {
+		t.Errorf("wrong completion result: %v vs %v", msg.Completion.Result, CompletionResultError)
+	}
+	if *msg.Completion.Error != errData {
+		t.Errorf("wrong completion data: %v vs %v", msg.Completion.Error, errData)
+	}
+}
+
+type testStatusData struct {
+	ProgressPercentage uint64 `json:"percentage"`
+}
+
+func TestReportStatus(t *testing.T) {
+	var W bytes.Buffer
+	var L bytes.Buffer
+	sink := Sink{
+		W: &W,
+		L: log.New(&L, "test: ", log.LstdFlags),
+	}
+	statusData := testStatusData{ProgressPercentage: 51}
+	sink.ReportStatus(&statusData)
+
+	var msg Status
+	dec := json.NewDecoder(&W)
+	dec.Decode(&msg)
+
+	if msg.Version != Version {
+		t.Errorf("wrong version: %v vs %v", msg.Version, Version)
+	}
+	if msg.ContentType != ContentTypeStatus {
+		t.Errorf("wrong contentType: %v vs %v", msg.ContentType, ContentTypeStatus)
+	}
+	if msg.Timestamp <= 0 {
+		t.Errorf("wrong timestamp: %v", msg.Timestamp)
+	}
+
+	// todo: msg.Status use interface{} to pass data
+}
